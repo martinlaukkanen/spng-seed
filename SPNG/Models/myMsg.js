3070b7545983aeea6e719e6220185be189a9544b
Martin <nospam@netverk.org>
2014-03-25 13:22:40 +0000

    Path updates

---
+++ SPNG/Models/myMsg.js
@@ -0,0 +1,19 @@
+﻿myApp.factory('myMsg', [function () {
+
+    // Object Model using Constructor / Prototype pattern
+    var MyMsg = function () {
+        this.message = 'default';
+
+        return this;
+    }
+
+    MyMsg.prototype.getMessage = function () {
+        return this.message;
+    }
+
+    MyMsg.prototype.setMessage = function (val) {
+        this.message = val;
+    }
+
+    return MyMsg;
+}]);
---
+++ SPNG/Services/SharePointService.js
@@ -0,0 +1,30 @@
+﻿myApp.factory('SharePointService', function ($q, $http) {
+    SP.SOD.executeOrDelayUntilScriptLoaded(Function.createDelegate(this, function() { 
+        
+        this.getCurrentUser = function () {
+            var deferred = $.Deferred();
+
+            var context = SP.ClientContext.get_current();
+            var user = context.get_web().get_currentUser(); 
+
+            context.load(user);
+            context.executeQueryAsync(
+                Function.createDelegate(this, function (sender, args) {
+                    // Success
+                    deferred.resolve(user);
+                }),
+                Function.createDelegate(this, function (sender, args) {
+                    // Failure
+                    deferred.reject('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+                })
+            );
+            
+            return deferred;
+        };
+
+    }), "SP.js");
+
+    return {
+        getCurrentUser: this.getCurrentUser
+    };
+});
